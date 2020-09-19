f4dcd830ac5e22a552b42cfb8171fd40187741bb
Miszczo <69727417+Miszczo@users.noreply.github.com>
2020-09-19 15:41:29 +0200

    Add class remover from all articles

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
 'use strict';
 
 function titleClickHandler(event){
-  const clickedElement = this;
   console.log('Link was clicked!');
-
-  /* [DONE] remove class 'active' from all article links  */
-
+  console.log(event);
+  /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
+  activeLink.classList.remove('active');
   }
+  /* add class 'active' to the clicked link */
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts .post.active');
 
-  console.log('clickedElement:', clickedElement);
-
-  /* [DONE] remove class 'active' from all articles */
-
+  for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+  }
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
@@ -29,3 +28,4 @@ const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+}
